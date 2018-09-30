d91bbace9de1cb19e11e84c502a842eea567d92b
Root on server steel.magehost.pro <root@steel.magehost.pro>
2018-09-30 22:50:50 +0200

    Extended load timings

--- loadtimes.js
+++ loadtimes.js
@@ -1,21 +1,24 @@
 #!/usr/bin/nodejs
 
+'use strict';
+
 const puppeteer = require('puppeteer');
 
-async function run() {
-  if ( process.argv.length < 3 ) {
+if ( process.argv.length < 3 ) {
     console.log("\n\tUsage: " + __filename + " [URL]\n");
     process.exit(-1);
-  }  
+}  
 
+(async() => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto( process.argv[2] );
-
-  const perfJson = await page.evaluate( () => JSON.stringify(performance.getEntriesByType('navigation')[0]) );
-  process.stdout.write( perfJson + "\n" );  
-
-  browser.close();
-}
-
-run();
+  console.log(
+      await page.evaluate( () => { 
+          var result = { 'timing': performance.timing,
+                         'entry': performance.getEntriesByType('navigation')[0] }; 
+          return JSON.stringify(result);
+      })
+  );
+  await browser.close();
+})();
