5708097d59f8940fa82570264af8053c0536e115
Jeroen Vermeulen - MageHost <jeroen@magehost.pro>
2018-11-29 14:52:13 +0100

    Improved error handling + ignore SSL errors

--- loadtimes.js
+++ loadtimes.js
@@ -4,23 +4,31 @@
 
 const puppeteer = require('puppeteer');
 
-if ( process.argv.length < 3 ) {
+if (process.argv.length < 3) {
     console.log("\n\tUsage: " + __filename + " [URL]\n");
     process.exit(-1);
-}  
+}
 
-(async() => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const defaultUserAgent = await page.evaluate('navigator.userAgent');
-  await page.setUserAgent(defaultUserAgent + ' - MageHost.pro Munin LoadTimes Measure');  
-  await page.goto( process.argv[2] );
-  console.log(
-      await page.evaluate( () => { 
-          var result = { 'timing': performance.timing,
-                         'entry': performance.getEntriesByType('navigation')[0] }; 
-          return JSON.stringify(result);
-      })
-  );
-  await browser.close();
+(async () => {
+    const browser = await puppeteer.launch( { ignoreHTTPSErrors: true} );
+    const page = await browser.newPage();
+    const defaultUserAgent = await page.evaluate('navigator.userAgent');
+    await page.setUserAgent(defaultUserAgent + ' - MageHost.pro Munin LoadTimes Measure');
+    page.goto(process.argv[2]).then(function () {
+        page.evaluate(() => {
+            return JSON.stringify({
+                'timing': performance.timing,
+                'entry': performance.getEntriesByType('navigation')[0]
+            });
+        }).then(function (json) {
+            console.log(json);
+            browser.close();
+        }).catch(function (err) {
+            console.log('ERROR Page Evaluate failed: ' + err);
+            browser.close();
+        });
+    }).catch(function (err) {
+        console.log('ERROR URL Load failed: ' + err);
+        browser.close();
+    });
 })();
